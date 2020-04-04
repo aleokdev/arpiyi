efe483261f7e1d56623ba6cf2a428f27637f98c5
alexdevteam <alexpro820@gmail.com>
2020-04-04 23:16:07 +0200

    Include plugin (oops, was ignored)

---
+++ build/editor/data/plugins/test.lua
@@ -0,0 +1,9 @@
+local toolbar = EditorToolbar.new("File", function(self)
+    print("hello!")
+    self:delete()
+end)
+
+local toolbar = EditorToolbar.new("Edit", function(self)
+    print("hello!")
+    self:delete()
+end)
