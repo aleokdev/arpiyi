b3b302ef644b8209e45523a01e39682e87ce91f1
alexdevteam <alexpro820@gmail.com>
2020-04-12 13:19:40 +0200

    CLion didn't add tileset_picker lol

---
+++ editor/src/widgets/tileset_picker.cpp
@@ -0,0 +1,23 @@
+#include "widgets/tileset_picker.hpp"
+#include "tileset_manager.hpp"
+
+#include <imgui.h>
+
+namespace arpiyi_editor::widgets::tileset_picker {
+
+void show(Handle<assets::Tileset>& tileset) {
+    auto t = tileset.get();
+    if (ImGui::BeginCombo("Tileset", t ? t->name.c_str() : "<Not selected>")) {
+        for (auto& ts : tileset_manager::get_tilesets()) {
+            std::string selectable_strid = std::to_string(ts.get_id()) + ts.get()->name;
+            if (ImGui::Selectable(selectable_strid.c_str(), ts == tileset))
+                tileset = ts;
+
+            if (ts == tileset)
+                ImGui::SetItemDefaultFocus();
+        }
+        ImGui::EndCombo();
+    }
+}
+
+}
