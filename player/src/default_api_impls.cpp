705eeb08c1d18948fa3d18bddc8bb44825f2e769
alexdevteam <alexpro820@gmail.com>
2020-04-26 20:34:38 +0200

    Render map sprites in player

--- player/src/default_api_impls.cpp
+++ player/src/default_api_impls.cpp
@@ -18,12 +18,16 @@
 namespace aml = anton::math;
 
 static arpiyi::Handle<arpiyi::assets::Shader> sprite_shader;
+static arpiyi::Handle<arpiyi::assets::Shader> tile_shader;
+static arpiyi::Handle<arpiyi::assets::Mesh> quad_mesh;
 static aml::Matrix4 proj_mat;
 
 namespace arpiyi::default_api_impls {
 
 void init() {
+    quad_mesh = asset_manager::put<assets::Mesh>(assets::Mesh::generate_quad());
     sprite_shader = asset_manager::load<assets::Shader>({"data/basic.vert", "data/basic.frag"});
+    tile_shader = asset_manager::load<assets::Shader>({"data/basic.vert", "data/tile_uv.frag"});
 }
 
 } // namespace arpiyi::default_api_impls
@@ -44,12 +48,13 @@ void render_map_layer(assets::Map const& map, assets::Map::Layer& layer) {
 
     const auto& cam = game_data_manager::get_game_data().cam;
 
-    float map_total_width = map.width * global_tile_size::get() * cam->zoom;
-    float map_total_height = map.height * global_tile_size::get() * cam->zoom;
+    const float map_total_width = map.width * global_tile_size::get() * cam->zoom;
+    const float map_total_height = map.height * global_tile_size::get() * cam->zoom;
 
     aml::Matrix4 model = aml::Matrix4::identity;
 
-    // Center map
+    // Center map (Map mesh goes from 0 to 1 so we need to translate accordingly)
+    // TODO: Make map mesh go from -0.5 to 0.5
     model *= aml::translate({-map_total_width / 2.f, map_total_height / 2.f, 0});
     // Translate by camera vector
     model *= aml::translate(aml::Vector3(cam->pos) * global_tile_size::get() * cam->zoom);
@@ -63,6 +68,56 @@ void render_map_layer(assets::Map const& map, assets::Map::Layer& layer) {
     glDrawArrays(GL_TRIANGLES, 0, map.width * map.height * quad_verts);
 }
 
+void render_map_entities(assets::Map const& map) {
+    const auto& cam = game_data_manager::get_game_data().cam;
+
+    const float map_total_width = map.width * global_tile_size::get() * cam->zoom;
+    const float map_total_height = map.height * global_tile_size::get() * cam->zoom;
+
+    for (const auto& entity_handle : map.entities) {
+        assert(entity_handle.get());
+        if (auto entity = entity_handle.get()) {
+            if (auto sprite = entity->sprite.get()) {
+                assert(sprite->texture.get());
+                glBindFramebuffer(GL_FRAMEBUFFER, 0);
+                glActiveTexture(GL_TEXTURE0);
+                glBindTexture(GL_TEXTURE_2D, sprite->texture.get()->handle);
+
+                glUseProgram(tile_shader.get()->handle);
+                glBindVertexArray(quad_mesh.get()->vao);
+
+                const auto size_in_pixels = sprite->get_size_in_pixels();
+                float sprite_total_width = size_in_pixels.x * cam->zoom;
+                float sprite_total_height = size_in_pixels.y * cam->zoom;
+
+                aml::Matrix4 model = aml::Matrix4::identity;
+
+                // Apply sprite pivot
+                model *= aml::translate(aml::Vector3(-sprite->pivot) /
+                                        aml::Vector3(map_total_width, map_total_height, 1));
+                // Map mesh goes from 0 to 1 so we need to translate accordingly
+                model *= aml::translate({-map_total_width / 2.f, -map_total_height / 2.f, 0});
+                // Translate by position of entity
+                model *=
+                    aml::translate(aml::Vector3(entity->pos) * global_tile_size::get() * cam->zoom);
+                // Translate by camera vector
+                model *=
+                    aml::translate(aml::Vector3(cam->pos) * global_tile_size::get() * cam->zoom);
+                // Scale accordingly
+                model *= aml::scale(aml::Vector3{sprite_total_width, sprite_total_height, 1});
+
+                glUniformMatrix4fv(1, 1, GL_FALSE, model.get_raw());
+                glUniformMatrix4fv(2, 1, GL_FALSE, proj_mat.get_raw());
+                glUniform2f(3, sprite->uv_min.x, sprite->uv_min.y);
+                glUniform2f(4, sprite->uv_max.x, sprite->uv_max.y);
+
+                constexpr int quad_verts = 2 * 3;
+                glDrawArrays(GL_TRIANGLES, 0, quad_verts);
+            }
+        }
+    }
+}
+
 void map_screen_layer_render_cb() {
     aml::Vector2 output_size = window_manager::get_framebuf_size();
     proj_mat = aml::orthographic_rh(-output_size.x / 2.f, output_size.x / 2.f, output_size.y / 2.f,
@@ -73,12 +128,14 @@ void map_screen_layer_render_cb() {
             assert(layer.get());
             render_map_layer(*map, *layer.get());
         }
+        render_map_entities(*map);
     }
 }
 
 KeyState get_key_state(InputKey key) {
     // TODO: Implement just_pressed and just_released
-    return {glfwGetKey(window_manager::get_window(), static_cast<int>(key)) == GLFW_PRESS, false, false};
+    return {glfwGetKey(window_manager::get_window(), static_cast<int>(key)) == GLFW_PRESS, false,
+            false};
 }
 
 } // namespace arpiyi::api
